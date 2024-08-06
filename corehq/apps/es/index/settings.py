37b0087acf5e95e6c6648048bf6bfadf9f911c81
Graham Herceg <gherceg@dimagi.com>
2024-08-06 11:57:46 -0400

    Unpack default slow config dict in ES settings

--- corehq/apps/es/index/settings.py
+++ corehq/apps/es/index/settings.py
@@ -138,11 +138,30 @@ DEFAULT = "default"
 
 DEFAULT_REPLICAS = 0
 DEFAULT_SHARDS = 5
+DEFAULT_SLOWLOG_CONFIG = {
+    "index.search.slowlog.threshold.query.warn": "10s",
+    "index.search.slowlog.threshold.query.info": "5s",
+    "index.search.slowlog.threshold.query.debug": "2s",
+    "index.search.slowlog.threshold.query.trace": "500ms",
+    "index.search.slowlog.threshold.fetch.warn": "1s",
+    "index.search.slowlog.threshold.fetch.info": "800ms",
+    "index.search.slowlog.threshold.fetch.debug": "500ms",
+    "index.search.slowlog.threshold.fetch.trace": "200ms",
+    "index.indexing.slowlog.threshold.query.warn": "10s",
+    "index.indexing.slowlog.threshold.query.info": "5s",
+    "index.indexing.slowlog.threshold.query.debug": "2s",
+    "index.indexing.slowlog.threshold.query.trace": "500ms",
+    "index.indexing.slowlog.threshold.fetch.warn": "1s",
+    "index.indexing.slowlog.threshold.fetch.info": "800ms",
+    "index.indexing.slowlog.threshold.fetch.debug": "500ms",
+    "index.indexing.slowlog.threshold.fetch.trace": "200ms",
+}
 
 DEFAULT_TUNING_SETTINGS = {
     DEFAULT: {
         IndexTuningKey.REPLICAS: DEFAULT_REPLICAS,
         IndexTuningKey.SHARDS: DEFAULT_SHARDS,
+        **DEFAULT_SLOWLOG_CONFIG,
     },
     IndexSettingsKey.APPS: {
         IndexTuningKey.REPLICAS: 0,
